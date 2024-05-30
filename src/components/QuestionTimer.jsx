0d2a271b927f67e3a47041d3d6aa368458f43f5c
ritacc12 <rita20920@gmail.com>
2024-05-30 18:33:29 +0800

    set clearTimeout and clearInterval to cancel the previously set timer to avoid potential memory leaks and unnecessary function calls

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-//timeout倒數時間 onTimeout啟動倒數時間
+//timeout倒數總時間 onTimeout倒計時結束時調用的回調函數
 const QuestionTimer = ({ timeout, onTimeout }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    console.log("setting timeout");
+    const timer = setTimeout(onTimeout, timeout);
+
+    //clearTimeout確保當組件重新渲染（例如 timeout 或 onTimeout發生變化時）或卸載時，不會有多餘的計時器在後台運行，避免潛在的內存洩漏和不必要的函數調用。
+    //清理函數會調用 clearTimeout 來取消之前設置的計時器
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
+  //每隔 100 毫秒更新一次 remainingTime。
   useEffect(() => {
-    setInterval(() => {
+    console.log("setting interval");
+
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
