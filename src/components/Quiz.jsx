2e1788fe3ca13b7912a0d64dc2c9704271dafd3e
ritacc12 <rita20920@gmail.com>
2024-05-30 17:34:57 +0800

    Shuffle answers and add quiz completion UI

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompletedIcon from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -12,12 +15,26 @@ const Quiz = () => {
     });
   }
 
+  //設定完成後畫面
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedIcon} alt="icon"></img>
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  //答案洗牌
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
