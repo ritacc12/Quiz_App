7ef7b987bdece88711d21452aeaff065798691ab
ritacc12 <rita20920@gmail.com>
2024-05-31 18:31:52 +0800

    split out the answer component to manage random answers and add different css styles for correct and incorrect answers

---
+++ src/components/Answers.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useRef } from "react";
+
+const Answers = ({ answers, selectedAnswer, answerState, onSelect }) => {
+  const shuffledAnswers = useRef();
+  //答案洗牌
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        let cssClass = "";
+        const isSelected = selectedAnswer === answer;
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssClass = answerState;
+        }
+        return (
+          <li key={answer} className="answer">
+            <button onClick={() => onSelect(answer)} className={cssClass}>
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Answers;
