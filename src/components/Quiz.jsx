287c49bd9b68e8643865129236e2232ba5522817
ritacc12 <rita20920@gmail.com>
2024-05-31 18:36:37 +0800

    import Answer & Questions component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,23 +2,41 @@ import React from "react";
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompletedIcon from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
+// import QuestionTimer from "./QuestionTimer.jsx";
+// import Answers from "./Answers.jsx";
+import Questions from "./Questions.jsx";
 
 const Quiz = () => {
-  const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const [userAnswers, setUserAnswers] = useState([]); //儲存用戶的答案
+  const [answerState, setAnswerState] = useState(""); //追蹤當前問題的答案狀態
+
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   //處理用戶選擇答案的操作 useCallback確保 handleSelectAnswer函數在組件的整個生命週期中保持不變，不會在每次重新渲染時重新創建。這對於性能優化特別有用，尤其是當這個函數被傳遞給子組件或用作依賴時。
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
-    setUserAnswers((prevUserAnswers) => {
-      return [...prevUserAnswers, selectedAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   //處理跳過答案的操作 handleSkipAnswer僅在 handleSelectAnswer 改變時才會重新創建，有助於性能優化。
   const handleSkipAnswer = useCallback(
@@ -36,30 +54,17 @@ const Quiz = () => {
     );
   }
 
-  //答案洗牌
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
-
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={10000}
-          //當用戶在指定時間內沒有選擇答案時，QuestionTimer 會自動調用 handleSkipAnswer，這樣當前問題會被標記為跳過, 確保測驗流程不會因為用戶沒有選擇答案而卡住。
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Questions
+        key={activeQuestionIndex}
+        questionText={QUESTIONS[activeQuestionIndex].text}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 };
