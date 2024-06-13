8f5498ae548b870b321c2068479cb6146e02837c
ritacc12 <rita20920@gmail.com>
2024-06-13 17:43:36 +0800

    Moving logic to question component

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionTimer from "./QuestionTimer.jsx";
 import Answers from "./Answers.jsx";
+import QUESTION from "../questions.js";
 
 const Questions = ({
-  questionText,
-  answers,
-  onSelect,
-  selectedAnswer,
-  answerState,
+  index,
+  // questionText,
+  // answers,
+  onSelectAnswer,
+  //selectedAnswer,
+  // answerState,
   onSkipAnswer,
 }) => {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTION[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
+
   return (
     <div id="question">
       <QuestionTimer
@@ -17,12 +49,12 @@ const Questions = ({
         //當用戶在指定時間內沒有選擇答案時，QuestionTimer 會自動調用 handleSkipAnswer，這樣當前問題會被標記為跳過, 確保測驗流程不會因為用戶沒有選擇答案而卡住。
         onTimeout={onSkipAnswer}
       />
-      <h2>{questionText}</h2>
+      <h2>{QUESTION[index].text}</h2>
       <Answers
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTION[index].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelect}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -8,35 +8,34 @@ import Questions from "./Questions.jsx";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]); //儲存用戶的答案
-  const [answerState, setAnswerState] = useState(""); //追蹤當前問題的答案狀態
+  // const [answerState, setAnswerState] = useState(""); //追蹤當前問題的答案狀態
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   //處理用戶選擇答案的操作 useCallback確保 handleSelectAnswer函數在組件的整個生命週期中保持不變，不會在每次重新渲染時重新創建。這對於性能優化特別有用，尤其是當這個函數被傳遞給子組件或用作依賴時。
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
-      setUserAnswers((prevUserAnswers) => {
-        return [...prevUserAnswers, selectedAnswer];
-      });
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    // setAnswerState("answered");
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
 
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
+    // setTimeout(() => {
+    //   if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+    //     setAnswerState("correct");
+    //   } else {
+    //     setAnswerState("wrong");
+    //   }
 
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+    //   setTimeout(() => {
+    //     setAnswerState("");
+    //   }, 2000);
+    // }, 1000);
+  },
+  []);
 
   //處理跳過答案的操作 handleSkipAnswer僅在 handleSelectAnswer 改變時才會重新創建，有助於性能優化。
   const handleSkipAnswer = useCallback(
@@ -58,11 +57,12 @@ const Quiz = () => {
     <div id="quiz">
       <Questions
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        answerState={answerState}
-        onSelect={handleSelectAnswer}
+        index={activeQuestionIndex}
+        // questionText={QUESTIONS[activeQuestionIndex].text}
+        // answers={QUESTIONS[activeQuestionIndex].answers}
+        // selectedAnswer={userAnswers[userAnswers.length - 1]}
+        // answerState={answerState}
+        onSelectAnswer={handleSelectAnswer}
         onSkipAnswer={handleSkipAnswer}
       />
     </div>
